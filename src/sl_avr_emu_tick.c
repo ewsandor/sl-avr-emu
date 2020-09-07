4f0b561a2b8c14580e857f6f33b2b335bd48ff4a
Ed Sandor <ed@ewsandor.com>
2020-09-06 23:18:47 -0600

    64bit tick counter prtinf

--- src/sl_avr_emu_tick.c
+++ src/sl_avr_emu_tick.c
@@ -1685,13 +1685,13 @@ sl_avr_emu_result_e sl_avr_emu_tick(sl_avr_emu_emulation_s * emulation)
   if(emulation->op_cycles_remaining > 0)
   {
     emulation->op_cycles_remaining--;
-    SL_AVR_EMU_VERBOSE_LOG(printf("tick %u: %u cycles for remaining for current operation\n", emulation->tick_count, emulation->op_cycles_remaining));
+    SL_AVR_EMU_VERBOSE_LOG(printf("tick %lu: %u cycles for remaining for current operation\n", emulation->tick_count, emulation->op_cycles_remaining));
   }
   else
   {
     if(SL_AVR_EMU_PC_ADDRESS_VALID(emulation->memory.pc))
     {
-      SL_AVR_EMU_VERBOSE_LOG(printf("tick %u: PC 0x%06x. OP 0x%04x.\n", emulation->tick_count, emulation->memory.pc, emulation->memory.flash[emulation->memory.pc]));
+      SL_AVR_EMU_VERBOSE_LOG(printf("tick %lu: PC 0x%06x. OP 0x%04x.\n", emulation->tick_count, emulation->memory.pc, emulation->memory.flash[emulation->memory.pc]));
       switch (emulation->memory.flash[emulation->memory.pc] >> (16-2)) {
         case 0b00:
         {
