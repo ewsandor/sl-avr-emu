8bddf60df87f1c30488f3415f859eb9d7b099db3
Ed Sandor <ed@ewsandor.com>
2020-09-09 22:52:51 -0600

    Make verbose logging optional with -v flag

--- src/sl_avr_emu.c
+++ src/sl_avr_emu.c
@@ -17,7 +17,7 @@
 #include "sl_avr_emu_hex.h"
 
 /* Global flag to enable/disable verbose logging */
-bool sl_avr_emu_verbose_logging_enabled = true;
+bool sl_avr_emu_verbose_logging_enabled = false;
 
 sl_avr_emu_result_e sl_avr_emu_init(sl_avr_emu_emulation_s *emulation)
 {
@@ -35,21 +35,30 @@ sl_avr_emu_result_e sl_avr_emu_init(sl_avr_emu_emulation_s *emulation)
 
 int main(int argc, char *argv[])
 {
-  sl_avr_emu_result_e    result;
+  uint32_t i;
+  sl_avr_emu_result_e    result = SL_AVR_EMU_RESULT_SUCCESS;
   sl_avr_emu_emulation_s emulation;
 
   sl_avr_emu_init(&emulation);
 
-  if(argc > 2)
+  for(i = 1; i < argc; i++)
   {
-    if(strcmp(argv[1],"-h") == 0)
+    if(strcmp(argv[i],"-v") == 0)
     {
-      result = sl_avr_emu_load_hex(&emulation, argv[2]);
-
-      if(result != SL_AVR_EMU_RESULT_SUCCESS)
+      printf("Verbose logging enabled.\n");
+      sl_avr_emu_verbose_logging_enabled = true;
+    }
+    else if(strcmp(argv[i],"-h") == 0)
+    {
+      if((i+1) < argc)
       {
-        fprintf(stderr, "Error! Failed to load hex %u at %s\n", result, argv[2]);
-        return result;
+        result = sl_avr_emu_load_hex(&emulation, argv[i+1]);
+
+        if(result != SL_AVR_EMU_RESULT_SUCCESS)
+        {
+          fprintf(stderr, "Error! Failed to load hex %u at %s\n", result, argv[2]);
+          return result;
+        }
       }
     }
   }
